40f377031300bb43a212bb58bfaaade2f8ea7b76
YoshiRi <yoshiyoshidetteiu@gmail.com>
2018-01-24 11:31:58 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+__author__ = 'Yoshi Ri'
+
+
+setup(
+    name='ImRegPOC',  
+    version='1.0',  
+    description='Image Registration Tool using 2D FFT',  
+    author='Yoshi Ri', 
+    author_email='yoshiyoshidetteiu@gmail.com',  
+    url='https://github.com/YoshiRi/ImRegPOC', 
+    classifiers=[ 
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    packages=find_packages(), 
+    include_package_data=True,  
+    keywords=['Image Registration', 'POC', 'FFT'], 
+    license='BSD License', 
+    install_requires=[ 
+        'cv2',
+        'numpy',
+        'matplotlib',
+        'sys',
+        'os',
+        'random',
+        'tkinter',
+    ],
+    entry_points="""  # コマンドラインにするときのエントリーポイント、pitchpx/__init__.pyの関数をエントリーポイントにしました.
+        [console_scripts]
+        pitchpx = pitchpx:main
+    """,
+)
